5de1f0ce1d23d876ab718b78630f1019e07da6f8
Morten Olsen <fbtijfdq@void.black>
2023-09-06 12:57:39 +0200

    chore: trying to ensure packages gets published

--- packages/playground/vite.config.ts
+++ packages/playground/vite.config.ts
@@ -2,7 +2,10 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react-swc';
 import mdx from '@mdx-js/rollup';
 
+const ASSET_URL = process.env.ASSET_URL || '';
+
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: `${ASSET_URL}/dist/`,
   plugins: [mdx(), react()],
 });
