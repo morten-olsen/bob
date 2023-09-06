b722d385f78357bfb4325b7051cc27af9ca73407
Morten Olsen <fbtijfdq@void.black>
2023-09-06 13:21:13 +0200

    ci: enable npm workspaces

---
+++ scripts/set-version.ts
@@ -0,0 +1,26 @@
+import { findWorkspacePackages } from '@pnpm/find-workspace-packages';
+import { writeFile } from 'fs/promises';
+import { join } from 'path';
+
+const version = process.argv[2];
+if (!version) {
+  throw new Error('Version is required');
+}
+
+const run = async () => {
+  const packages = await findWorkspacePackages(process.cwd());
+
+  for (const { manifest, dir } of packages) {
+    console.log(dir, version);
+    manifest.version = version;
+    await writeFile(
+      join(dir, 'package.json'),
+      JSON.stringify(manifest, null, 2),
+    );
+  }
+};
+
+run().catch((err) => {
+  console.error(err);
+  process.exit(1);
+});
